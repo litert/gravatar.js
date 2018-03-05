9db097182fc4926c880fda360c922f0444a3c1f8
Angus.Fenying <i.am.x.fenying@gmail.com>
2018-03-05 17:21:48 +0800

    Update copyrights info.

--- sources/test.ts
+++ sources/test.ts
@@ -2,14 +2,14 @@
    +----------------------------------------------------------------------+
    | LiteRT Gravatar.js Library                                           |
    +----------------------------------------------------------------------+
-   | Copyright (c) 2017 LiteRT.org                                        |
+   | Copyright (c) 2018 Fenying Studio                                    |
    +----------------------------------------------------------------------+
-   | This source file is subject to the MIT license, that is bundled with |
-   | this package in the file LICENSE, and is available through the       |
-   | world-wide-web at the following url:                                 |
+   | This source file is subject to version 2.0 of the Apache license,    |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
    | https://github.com/litert/gravatar.js/blob/master/LICENSE            |
    +----------------------------------------------------------------------+
-   | Authors: Angus Fenying <i.am.x.fenying@gmail.com>                    |
+   | Authors: Angus Fenying <fenying@litert.org>                          |
    +----------------------------------------------------------------------+
  */
 
